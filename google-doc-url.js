b64a32c325d8e4f5605dfcd1bfc0e88f4bd88bbc
facundoibarlucia <49283851+facundoibarlucia@users.noreply.github.com>
2023-11-24 12:46:55 -0300

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/18zhJ8wBj6KnrzUVSLvPWUGdzmg0iGHKx4fRZ5A6l8uA/edit?usp=sharing';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit?usp=sharing';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyCFuhM6rCv2e44UFamHGWvpo2wblaCLoB4';
